393a602a3765689206f2f81c3277b3be1b6c7030
Andy Agus <Redagus@gmail.com>
2020-06-24 13:19:21 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.destroy_all
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
+Coupon.create(coupon_code:rand(1..1000), store:Faker::FunnyName.name)
